956dda7f838a8f1eb64a0bd2c1d2f47e1cd0f809
Florian POUCHELET <pouchelet.florian@orange.fr>
2018-03-06 14:11:47 +0100

    refactor(User): schema des user mis à jour

--- ModelEntities/user.cs
+++ ModelEntities/user.cs
@@ -1,17 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/// <remarks>
+/// Florian POUCHELET
+/// </remarks>
 
 namespace ModelEntities
 {
-    class User : IUser
+    public class User
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Tel { get; set; }
-        public Email EmailAddress { get; set; }
-        public string Password { get; }
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+        public string Society { get; set; }
+        public bool IsAdmin { get; set; }
+
+        public User(string fName, string lName, string pwd, bool isAdmin)
+        {
+            FirstName = fName;
+            LastName = lName;
+            Password = pwd;
+            IsAdmin = isAdmin;
+        }
     }
 }
