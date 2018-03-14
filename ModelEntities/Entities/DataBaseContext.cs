79bb42d6eab1b7cd57846848e223c578f46c8e85
Florian POUCHELET <pouchelet.florian@orange.fr>
2018-03-14 16:02:49 +0100

    feat(Entities): ajoute le contexte de base de données

---
+++ ModelEntities/Entities/DataBaseContext.cs
@@ -0,0 +1,23 @@
+﻿/// <remarks>
+/// Florian POUCHELET
+/// </remarks>
+/// <summary>
+/// Créé le context nécessaire à notre application
+/// </summary>
+
+using System.Data.Entity;
+
+namespace ModelEntities.Entities
+{
+    class DataBaseContext : DbContext
+    {
+        public DataBaseContext() : base("QuizzApplication")
+        {
+            //Pour la création de la base
+            Database.SetInitializer(new DropCreateDatabaseAlways<DataBaseContext>());
+            
+            //Pour la migration
+            //Database.SetInitializer(new MigrateDatabaseToLatestVersion<DataBaseContext, System.Data.Entity.Migrations.Configuration>()); 
+        }
+    }
+}
