6ff6c6de8f366ae28e170ef65663be4a99f80472
Florian POUCHELET <pouchelet.florian@orange.fr>
2018-03-30 13:49:30 +0200

    refactor(Entities): corrige un pb de multiplicité dans le schéma

---
+++ ModelEntities/Entities/Result.cs
@@ -0,0 +1,32 @@
+﻿/// <remarks>
+/// Florian POUCHELET
+/// </remarks>
+/// <summary>
+/// Représente le résultat à un quizz
+/// </summary>
+
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ModelEntities.Entities
+{
+    public class Result
+    {
+        #region Properties
+        /* membres de classe propres à l'objet */
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ResultID { get; set; }
+
+        [ForeignKey("LinkedQuizz")]
+        public int QuizzID { get; set; }
+        #endregion
+
+        #region Associations
+        [Required]
+        public virtual Quizz LinkedQuizz { get; set; }
+        public virtual ResultAnswer LinkedResultAnswers { get; set; }
+        #endregion
+    }
+}
