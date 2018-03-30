e5e0232ab468147dbbf487fc4b11901f17d35fff
romain.ruelle-petel <ruellepetelromain@gmail.com>
2018-03-30 16:36:39 +0200

    feat(ViewModels) : ajoute / modifie les vues modèles

---
+++ ModelEntities/ModelViews/MasterDifficultyViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelEntities.ModelViews
+{
+    class MasterDifficultyViewModel
+    {
+        [Display(Name = "Master Difficulty ID : ")]
+        public int MasterDifficultyID { get; set; }
+
+        [Required]
+        [Display(Name = "Libellé : ")]
+        public string Wording { get; set; }
+
+        [Required]
+        [Display(Name = "Type de Difficulté : ")]
+        public string DifficultyType { get; set; }
+    }
+}
