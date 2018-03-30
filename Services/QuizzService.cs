01f754f9559e6aba1e830e4f885f7c85f174a4d4
Anthony <anthony-cleradin@hotmail.fr>
2018-03-30 13:49:56 +0200

    feat(QuizzService): Ajout de 5 nouvelles méthodes

--- Services/QuizzService.cs
+++ Services/QuizzService.cs
@@ -1,20 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Linq;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ModelEntities.Entities;
+
 
 namespace Services
 {
     public class QuizzService
     {
 
+        private static DataBaseContext db = new DataBaseContext();
         /* 
-         * CreateQuiz : Créer le quiz et les questions liées
-         * UpdateQuizAnswer : sauvegarder la(es) réponse(s) pour une question donnée
-         * UpdateQuizAnswers : sauvegarder la(es) réponse(s) pour toute les questions du Quiz.
-         * GetQuizById() : retourner un Quiz par id
-         * GetAllQuizzes() : retourner la liste de tous les Quizz
+         
+        
+         
+         
+        
          */
+         /* GetAllQuizzes() : retourner la liste de tous les Quizz */
+        public static List<Quizz> GetAllQuizzes()
+        {
+            return db.Quizzes.ToList();
+        }
+
+        /* GetQuizById() : retourner un Quiz par id */
+        public static Quizz GetQuizById(int Id)
+        {
+            Quizz rQuizz = new Quizz();
+            using (db)
+            {
+                rQuizz = db.Quizzes.Single(m => m.QuizzID == Id);
+            }       
+            return rQuizz;
+        }
+
+        /* CreateQuiz : Créer le quiz et les questions liées*/
+        public void CreateQuiz(Quizz unQuizz)
+        {
+            using (db)
+            {
+                db.Quizzes.Add(unQuizz);
+                db.SaveChanges();
+            }
+        }
+
+        /* UpdateQuizAnswer : sauvegarder la(es) réponse(s) pour une question donnée*/
+        public void AddQuizAnswer(int idQuestion, Answer unResult)
+       {
+           using (db)
+           {
+               db.Answers.Add(unResult);
+               db.SaveChanges();
+           }
+
+       }
+
+       /* UpdateQuizAnswers : sauvegarder la(es) réponse(s) pour toute les questions du Quiz.*/
+        public void AddQuizAnswers(List<Answer> desResult)
+        {
+            using (db)
+            {
+                foreach (var result in desResult)
+                {
+                    db.Answers.Add(result);
+                    db.SaveChanges();
+                }
+               
+            }
+
+        }
     }
 }
