98a1f8535bf348003e0b9b19d4a6583890bcdc27
Florian POUCHELET <pouchelet.florian@orange.fr>
2018-04-03 09:27:52 +0200

    style(QuizzService): supprime les using inutiles

--- Services/QuizzService.cs
+++ Services/QuizzService.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Linq;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using ModelEntities.Entities;
 
 
