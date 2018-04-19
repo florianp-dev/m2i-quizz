2cb4a66c7c49a3c02ef75db5b9ec1fd77716428b
Florian POUCHELET <pouchelet.florian@orange.fr>
2018-04-19 16:54:42 +0200

    style(ModelsViews): enlève les using inutiles

--- ModelEntities/ModelViews/MasterDifficultyViewModel.cs
+++ ModelEntities/ModelViews/MasterDifficultyViewModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace ModelEntities.ModelViews
 {
@@ -19,6 +14,5 @@ namespace ModelEntities.ModelViews
         [Required]
         [Display(Name = "Id Pourcentage : ")]
         public string PercentID { get; set; }
-
     }
 }
