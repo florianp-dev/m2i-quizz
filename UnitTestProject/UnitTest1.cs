d7b2db7853af40406011b4ca105d777c042594e0
Florian POUCHELET <pouchelet.florian@orange.fr>
2018-04-24 15:13:28 +0200

    fix(Tests): supprime le corps devenu inutile

--- UnitTestProject/UnitTest1.cs
+++ UnitTestProject/UnitTest1.cs
@@ -10,53 +10,9 @@ namespace UnitTestProject
     public class UnitTest1
     {
         [TestMethod]
-        public void TestAddUserAndRoles()
+        public void TestMethod()
         {
-            var context = new DataBaseContext();
-
-            context.Roles.AddOrUpdate(r => r.Name, new IdentityRole("Admin"));
-            context.Roles.AddOrUpdate(r => r.Name, new IdentityRole("Recruitment Agent"));
-
-            context.SaveChanges();
-
-            var admin = new User
-            {
-                UserName = "admin",
-                FirstName = "Admin",
-                LastName = "User",
-                Email = "xxx@xxx.net",
-                PhoneNumber = "5551234567"
-            };
-            var rAgent = new User
-            {
-                UserName = "Recruitment Agent",
-                FirstName = "Recruitment",
-                LastName = "Agent",
-                Email = "yyy@xxx.net",
-                PhoneNumber = "5551234568"
-            };
-
-            context.Users.AddOrUpdate(u => u.UserName, admin);
-            context.Users.AddOrUpdate(u => u.UserName, rAgent);
-
-            context.SaveChanges();
-
-            var userStore = new UserStore<User>(context);
-            var userManager = new UserManager<User>(userStore);
-
-            userStore.AddToRoleAsync(admin, "Admin").Wait();
-            userStore.AddToRoleAsync(rAgent, "Recruitment Agent").Wait();
-
-
-            context.SaveChanges();
-
-            // mot de passe "admin"
-            /*userManager.Create(admin, "Admin#1");
-            userStore.AddToRoleAsync(admin, "Admin").Wait();
-
-            // mot de passe "agent"
-            userManager.Create(rAgent, "Agent#1");
-            userStore.AddToRoleAsync(rAgent, "Recruitment Agent").Wait();*/
+            
         }
     }
 }
