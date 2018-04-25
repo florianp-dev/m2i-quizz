6f44bee30825130645556de53729e680414f9160
Unknown <ruellepetelromain@gmail.com>
2018-04-25 16:38:44 +0200

    feat(Question): ajoute le controller question à l'API

---
+++ [FilRouge.API]Quizz/Controllers/QuestionsController.cs
@@ -0,0 +1,43 @@
+﻿using ModelEntities.ModelViews;
+using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FilRouge.API.Controllers
+{
+    public class QuestionsController : ApiController
+    {
+        private static readonly QuestionService _service = new QuestionService();
+
+        // GET: api/Questions
+        public List<QuestionViewModel> Get()
+        {
+            return _service.GetAllQuestions();
+        }
+
+        // GET: api/Questions/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Questions
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Questions/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Questions/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
