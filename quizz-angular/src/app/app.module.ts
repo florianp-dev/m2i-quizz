bef24ef7dd47a3b105e492f93eb1d9a9aca0812f
Florian POUCHELET <pouchelet.florian@orange.fr>
2018-05-03 12:24:51 +0200

    feat(app.angular): enregistre les services

--- quizz-angular/src/app/app.module.ts
+++ quizz-angular/src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import { AnswerComponent } from './components/answer/answer.component';
+import {AnswersService} from './Services/answers.service';
+import { QuestionsComponent } from './components/questions/questions.component';
+import {QuestionsService} from './Services/questions.service';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnswerComponent,
+    QuestionsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    AnswersService,
+    QuestionsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
