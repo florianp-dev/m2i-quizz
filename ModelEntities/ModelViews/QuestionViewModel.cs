1960b9fef2bda28a7ad866d054c63ea0233c3eb6
Florian POUCHELET <pouchelet.florian@orange.fr>
2018-05-04 11:29:38 +0200

    style(Question): supprime les using useless

--- ModelEntities/ModelViews/QuestionViewModel.cs
+++ ModelEntities/ModelViews/QuestionViewModel.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 using ModelEntities.Entities;
 
 namespace ModelEntities.ModelViews
 {
     public class QuestionViewModel
     {
-        
         [Display(Name = "ID Question : ")]
         public int QuestionID { get; set; }
 
